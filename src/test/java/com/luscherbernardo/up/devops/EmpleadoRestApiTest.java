219d317d6d45f449bfedd6f739c0174e7447d3ff
Bernardo Luscher <b_luscher@hotmail.com>
2023-10-05 17:24:53 -0300

    remove componentes no usados

--- src/test/java/com/luscherbernardo/up/devops/EmpleadoRestApiTest.java
+++ src/test/java/com/luscherbernardo/up/devops/EmpleadoRestApiTest.java
@@ -1,25 +1,17 @@
 package com.luscherbernardo.up.devops;
 
-import com.luscherbernardo.up.devops.empleado.Empleado;
 import com.luscherbernardo.up.devops.empleado.EmpleadoController;
 import com.luscherbernardo.up.devops.empleado.EmpleadoRepository;
 import com.luscherbernardo.up.devops.service.EmpleadoService;
-import com.luscherbernardo.up.devops.service.EmpleadoServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.List;
 
-import static org.mockito.Mockito.validateMockitoUsage;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
