92a1d556ed49c72ff39f24e5d078e3579b278ba1
Bernardo Luscher <b_luscher@hotmail.com>
2023-10-10 16:01:28 -0300

    add sql files to test config

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- data.sql
+INSERT INTO empleados (nombre, email, puesto) VALUES('test1', '@test1.com', 'puesto1');
+INSERT INTO empleados (nombre, email, puesto) VALUES('test2', '@test2.com', 'puesto2');
+INSERT INTO empleados (nombre, email, puesto) VALUES('test3', '@test3.com', 'puesto3');
+INSERT INTO empleados (nombre, email, puesto) VALUES('test4', '@test4.com', 'puesto4');
